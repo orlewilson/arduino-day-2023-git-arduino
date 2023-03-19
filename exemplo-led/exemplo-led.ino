5522b0362b10dd1d238d552f8cd96e5add95578e
Orlewilson Maia <orlewilson.maia@gmail.com>
2023-03-19 19:31:06 -0400

    Arquivo inicial do Projeto Exemplo LED
    Signed-off-by: Orlewilson Maia <orlewilson.maia@gmail.com>

---
+++ exemplo-led/exemplo-led.ino
@@ -0,0 +1,20 @@
+/*
+Arduino Day 2023
+
+Oficina:      Git + Arduino
+Facilitador:  Orlewilson Bentes Maia
+Autor:        informe seu nome
+Data:         19/03/2023
+*/
+
+// Funcao para configurar o Arduino
+void setup() {                
+
+}
+
+// Funcao principal do Arduino que ficara em loop infinito
+void loop() {
+
+}
+
+
